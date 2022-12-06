b7cd6cc5e30de26970c30a7e18379a16730334b9
Tyler Teschon <tyler.teschon@gmail.com>
2022-12-06 15:14:20 -0500

    Chat GPT suggestions
    Below suggestions from OpenAI's chat GPT integrated,
    
    Here are a few suggestions to refine the code:
    
    Instead of using a hardcoded string for the output file name ("merged-pdf.pdf"), you could use the name of the input folder, followed by the ".pdf" extension. For example, if the input folder is named "input_folder", the output file would be named "input_folder.pdf".
    
    You could add a check to ensure that the input folder contains at least one PDF file before starting the merge process. This will prevent the program from creating an empty output file if no PDFs are found.
    
    Instead of using the print function to display messages to the user, you could use the logging module. This will allow you to specify different levels of log messages (e.g. info, warning, error) and make it easier to read and understand the output.
    
    You could add error handling to the code to handle cases where the input folder does not exist, or if there is an error during the merge process. This will make the code more robust and prevent the program from crashing in case of unexpected input.

--- PDFMerge/PDFMerger.py
+++ PDFMerge/PDFMerger.py
@@ -1,29 +1,61 @@
 #!/usr/bin/env python3
 
 #import libraries
-from operator import concat
 import os
+import logging
 from PyPDF2 import PdfMerger
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
 #function that prompts user to select folder path for input
 def GetFolderPath():
-	path = askdirectory(title='Select Folder') # shows dialog box and return the path
-	return path
+    path = askdirectory(title='Select Folder') # shows dialog box and return the path
+    return path
 
 #function that loops through all pdf's in the folder and merges them
 def MergePDFsInFolder(path):
-	#create merger obejct
-	merger = PdfMerger()
-	for pdf in os.listdir(path):
-		print(path + pdf)
-		if pdf.endswith(".pdf"):
-			merger.append(path + "/" + pdf)
-		else:
-			continue
-	# Write to an output PDF document
-	merger.write(path + "/" + "merged-pdf.pdf")
-	# Close File Descriptor
-	merger.close()
-	print("merge complete")
+    # create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    # create console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+
+    # check if input folder exists
+    if not os.path.isdir(path):
+        logger.error("Input folder does not exist")
+        return
+
+    # create merger object
+    merger = PdfMerger()
+    pdf_count = 0
+    for pdf in os.listdir(path):
+        if pdf.endswith(".pdf"):
+            merger.append(path + "/" + pdf)
+            pdf_count += 1
+        else:
+            continue
+
+    # check if at least one PDF was found
+    if pdf_count == 0:
+        logger.warning("No PDFs found in input folder")
+        return
+
+    # create output file name
+    output_file = os.path.basename(os.path.normpath(path)) + ".pdf"
+
+    # write to an output PDF document
+    try:
+        merger.write(path + "/" + output_file)
+    except Exception as e:
+        logger.error("Error while writing to output file: {}".format(e))
+        return
+
+    # close file descriptor
+    merger.close
